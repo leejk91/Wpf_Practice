41a39baec7058d22f3bc9ffb04be5ce081f00610
이정기 <jk@ubcn.co.kr>
2023-05-08 16:29:31 +0900

    Season3_Day2_1

---
+++ WpfPracticeApp_season3_day2_1/WpfPracticeApp/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfPracticeApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            lbx.ItemsSource = GetSamples();
+        }
+
+        private IEnumerable GetSamples()
+        {
+            List<User> users = new();
+            users.Add(new User().GenData("Lee","Seoul"));
+            users.Add(new User().GenData("Lee2","Busan"));
+            users.Add(new User().GenData("Lee3","Paris"));
+            users.Add(new User().GenData("Lee4","Home"));
+
+            return users;
+        }
+    }
+}
