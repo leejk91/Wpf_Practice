0cf199d7b76f1779a92ed25ea7fd4eae60d4eb48
Jeongki <jeongki91@gmail.com>
2023-05-13 15:29:58 +0900

    Update

---
+++ Seaon3_Day3/LanguageApp/LanguageModel.cs
@@ -0,0 +1,14 @@
+﻿namespace LanguageApp
+{
+    public class LanguageModel
+    {
+        public string Code { get; private set; }
+        public string Name { get; private set; }
+
+        internal LanguageModel DataGen(string code, string name)
+        {
+            Code = code;
+            Name = name;
+        }
+    }
+}
---
+++ Seaon3_Day3/LanguageApp/MainViewModel.cs
@@ -0,0 +1,43 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LanguageApp
+{
+    public class MainViewModel : ObservableObject
+    {
+        public List<LanguageModel> Languages { get; set; }
+
+        private LanguageModel _languageModel;
+        public LanguageModel LanguageModel
+        {
+            get => _languageModel;
+            set => SetProperty2(ref _languageModel, value, LanguageModelChanged);
+        }
+
+        private void SetProperty2<T>(ref T value1, T value2, Action<T> changed)
+        {
+            value1 = value2;
+
+        }
+
+        private void LanguageModelChanged(LanguageModel obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public MainViewModel()
+        {
+            Languages = GetLanguage();
+        }
+
+        private List<LanguageModel> GetLanguage()
+        {
+            List<LanguageModel> source = new();
+            return source;
+        }
+    }
+}
